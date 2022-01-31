528807e2c3b6f77cafbc8d09c002616e4603cf79
NicoFernando19 <nicofernando39@gmail.com>
2022-01-31 11:55:44 +0700

    Email layout

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a href="https://fethesis.herokuapp.com/" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@else
+@lang('OutWeb.')
+@endif
+</a>
+</td>
+</tr>
