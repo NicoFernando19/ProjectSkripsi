bf23068ea895054a029ecb0535c21153193da8fa
NicoFernando19 <nicofernando39@gmail.com>
2022-04-09 16:34:28 +0700

    profile fix

--- components/Modals/UpdateImageCompany.vue
+++ components/Modals/UpdateImageCompany.vue
@@ -83,9 +83,9 @@ export default {
                 Cookie.remove("authimgName");
                 Cookie.set("authimgName", res.data.imgName, { expires: 1 });
                 this.$bvModal.hide(this.id)
-                console.log(this.$root)
-                if (this.$root.$children[2].$refs) {
-                    await this.$root.$children[2].$refs.navbar.getAuth();
+                console.log(this.$root.$children[1].$refs)
+                if (this.$root.$children[1].$refs) {
+                    await this.$root.$children[1].$refs.navbar.getAuth();
                 }
                 Toast.showToast("Updated Data","Image profile updated successfully", "success");
                 this.$parent.getData()
